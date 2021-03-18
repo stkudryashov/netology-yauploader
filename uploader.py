0edbdb5e03c763e9fb3f8da4bf42c6fa56f77e31
stkudryashov <aleks59niki30@gmail.com>
2021-03-18 18:29:09 +0300

    class uploader init

---
+++ uploader.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str):
+        """Метод загруджает файл file_path на яндекс диск"""
+        # Тут ваша логика
+        return 'Вернуть ответ об успешной загрузке'
+
+
+if __name__ == '__main__':
+    uploader = YaUploader('<Your Yandex Disk token>')
+    result = uploader.upload('c:\my_folder\file.txt')
